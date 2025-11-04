6ea9cec282c768e0d14ebea1788810c076b1f802
csdarren <2001dhall@gmail.com>
2025-11-04 16:49:00 -0500

    updated dependencies

--- main/drybox_temp.cpp
+++ main/drybox_temp.cpp
@@ -7,7 +7,6 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 #include "aht20.h"
-#include "ssd1306.h"
 
 #include "font8x8_basic.h"
 
@@ -49,44 +48,9 @@ using AHT20_t = struct {
 };
 
 void i2c_task(void *pvParameter) {
-    // Register the device with i2c address = 0c3C
-    SSD1306_t dev1;
     AHT20_t aht1;
-    i2c_master_init(&dev1, CONFIG_SDA_GPIO, CONFIG_SCL_GPIO, CONFIG_RESET_GPIO);
-
-    // Show Device 1
-    ssd1306_init(&dev1, 128, 64);
-    ssd1306_clear_screen(&dev1, false);
-    ssd1306_contrast(&dev1, 0xff);
-
-    aht20_dev_handle_t aht20_handle = aht20_i2c_init(dev1._i2c_bus_handle); // piggybacking off SSD1306 i2c handle
-
-    uint32_t temperature_raw = 0;
-    uint32_t humidity_raw = 0;
-    float temperature = 0.0;
-    float humidity = 0.0;
-
-    aht20_read_temperature_humidity(aht20_handle, &temperature_raw, &temperature, &humidity_raw, &humidity);
-
-    ssd1306_display_text_x3(&dev1, 0, "", 5, false);
-
-    ESP_LOGI("TEMPS", "%-20s: %2.2f %%", "humidity is", humidity);
-    ESP_LOGI("TEMPS", "%-20s: %2.2f degC", "temperature is", temperature);
-
-    // // Show Device 2
-    // ssd1306_init(&dev2, 128, 64);
-    // ssd1306_clear_screen(&dev2, false);
-    // ssd1306_contrast(&dev2, 0xff);
-    // ssd1306_display_text_x3(&dev2, 0, "World", 5, false);
-
-    vTaskDelete(nullptr);
 }
 
 extern "C" void app_main() {
-    ESP_LOGI(tag, "INTERFACE is i2c");
-    ESP_LOGI(tag, "CONFIG_SDA_GPIO=%d", CONFIG_SDA_GPIO);
-    ESP_LOGI(tag, "CONFIG_SCL_GPIO=%d", CONFIG_SCL_GPIO);
-    ESP_LOGI(tag, "CONFIG_RESET_GPIO=%d", CONFIG_RESET_GPIO);
-
     xTaskCreate(&i2c_task, "I2C", 1024 * 6, nullptr, 2, nullptr);
 }
